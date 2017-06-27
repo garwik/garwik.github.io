da72b10bcfb3de940a07e72f58ca201b7afba480
ronan <ronanstclair@yahoo.ca>
2017-06-27 13:32:31 -0300

    email?

--- submissionpage.php
+++ submissionpage.php
@@ -1,7 +1,5 @@
 <?php
-  $name = $_REQUEST['name'];
-  $pass = $_REQUEST['pass'];
-
-  echo("name: " . $name);
-  echo("<br/>pass: " . $pass);
+if($_POST["admittance"]) {
+    mail("collective@yopmail.com", "Form to email message", $_POST["admittance"], "From: an@email.address");
+}
 ?>
