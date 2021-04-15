7a37ea4fc9b63854b1ea2f52c3c3241800a3505c
ronan <ronanstclair@yahoo.ca>
2021-04-15 00:27:42 -0300

    Revert "back and gallery pages"
    This reverts commit 2869ddf287923b8dc3ac159c98809eac65f224ef.

--- js/gallery.js
+++ js/gallery.js
@@ -1,46 +1,16 @@
 var dataTableContainer = document.getElementById("g-table-container");
-var fwdButton = document.getElementById("g-forwards");
-var bckButton = document.getElementById("g-backwards");
 
-var n = 4;
-var curr = 0;
-var spare = 0;
+console.log(dataTableContainer);
 
-//go through array and display n images
-function forward()
+var insert = "<table>";
+
+
+for(let i=0; i<data.length; i++)
 {
-	let insert = "<table>";
-	for(let i=0; i<n; i++)
-	{
-		if(curr >= data.length) 
-		{
-			spare = n-i;
-			break;
-		}
-		insert += "<tr><td>";
-		insert += "<a href=\"images/" + data[curr].link + ".png\" target=\"_blank\">";
-		insert += "<img src=\"images/" + data[curr].link + ".png\">";
-		insert += "</a>";
-		insert += "<p>" + data[curr].name + "</p>";
-		insert += "</td></tr>";
-		curr++;
-	}
-	dataTableContainer.innerHTML = insert;
-	
-	(spare != 0 || curr == data.length) ? fwdButton.disabled = true : fwdButton.disabled = false;
-	(curr == n) ? bckButton.disabled = true : bckButton.disabled = false;
+	insert += "<tr><td>";
+	insert += "<img src=\"images/" + data[i].link + ".png\">";
+	insert += "<p>" + data[i].name + "</p>";
+	insert += "</td></tr>";
 }
 
-function back()
-{
-	(spare != 0) ? curr -= n + spare : curr -= n*2;
-	if(curr < 0)
-	{
-		curr = 0;
-	}
-	spare = 0;
-	forward();
-}
-
-
-forward();
+dataTableContainer.innerHTML = insert;
--- js/gallerydata.js
+++ js/gallerydata.js
@@ -1,30 +1,12 @@
 var data = [
-	
 	{
-		"name": "serpent 2",
-		"link": "2021-04-14-2"
-	},
-	{
-		"name": "serpent 1",
-		"link": "2021-04-14"
+		"name": "xxx",
+		"link": "2020-01-28"
 	},
 	{
 		"name": "falling",
 		"link": "2021-03-24-2"
-	},
-	{
-		"name": "rainbows",
-		"link": "2021-03-01"
-	},
-	{
-		"name": "ttt",
-		"link": "2021-02-08"
-	},
-	{
-		"name": "xxx",
-		"link": "2020-01-28"
 	}
-	
 ]
 
 jsonData = JSON.stringify(data);
